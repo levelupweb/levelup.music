a14f2f82524823e6e8676e91d0c90921f8a8bfe8
kv9991 <exide-pra@yandex.ru>
2017-08-19 01:31:17 +0300

    emergency fixes

--- src/containers/RouteDefaultContainer.js
+++ src/containers/RouteDefaultContainer.js
@@ -8,23 +8,11 @@ const RouteDefaultContainer = ({ component }) =>
 	<Route
 		render={props => {
 			return (
-<<<<<<< HEAD
 				<div className="DefaultContainer">
 					{React.createElement(component)}
-=======
-				<div className="DefaultContainer container">
-					<Header />
-					<div className="container-content">
-						{React.createElement(component)}
-					</div>
->>>>>>> b8554f3a8368109e8209cd108b6e793412214281
 				</div>
 			);
 		}}
 	/>;
-
-RouteDefaultContainer.propTypes = {
-	component: propTypes.func.isRequired
-}
 
 export default RouteDefaultContainer;
